8f4608c19e947febc03dcc3e95cd5ae86dedaf0d
a-milek <44343010+a-milek@users.noreply.github.com>
2025-05-30 12:38:00 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,29 +1,3 @@
-
-Authentication is required to manage system service or unit files.
-Authenticating as: ,,, (amilek)
-Password:
-==== AUTHENTICATION COMPLETE ====
-==== AUTHENTICATING FOR org.freedesktop.systemd1.reload-daemon ====
-Authentication is required to reload the systemd state.
-Authenticating as: ,,, (amilek)
-Password:
-==== AUTHENTICATION COMPLETE ====
-chown: changing ownership of '/home/amilek/Documents/Server/WebControls/myenv/include/python3.11': Operation not permitted
-chown: changing ownership of '/home/amilek/Documents/Server/WebControls/myenv/include': Operation not permitted
-chown: changing ownership of '/home/amilek/Documents/Server/WebControls/myenv/lib/python3.11/site-packages/_distutils_hack/__pycache__/__init__.cpython-311.pyc': Operation not permitted
-chown: changing ownership of '/home/amilek/Documents/Server/Websocket/venv/include/python3.11': Operation not permitted
-chown: changing ownership of '/home/amilek/Documents/Server/Websocket/venv/include': Operation not permitted
-chown: changing ownership of '/home/amilek/Documents/Server/Websocket/venv/lib/python3.11/site-packages/_distutils_hack/__pycache__/__init__.cpython-311.pyc': Operation not permitted
-Setup complete.
-
-amilek@raspberrypi:~/Documents/Server $ ./serverscript.sh
-Traceback (most recent call last):
-  File "/home/amilek/Documents/Server/WebControls/main.py", line 5, in <module>
-    from Arduino import Arduino
-ModuleNotFoundError: No module named 'Arduino'
-
-amilek@raspberrypi:~/Documents/Server $
-  GNU nano 7.2                  /home/amilek/Documents/Server/install.sh
 #!/bin/bash
 
 # Ustaw użytkownika i ścieżki
