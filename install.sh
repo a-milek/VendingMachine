06ea36b0ff46aec970bddadd87a99145dfdc6c76
a-milek <44343010+a-milek@users.noreply.github.com>
2025-05-30 14:58:19 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-# Ustaw użytkownika i ścieżki
 USER="amilek"
 USER_HOME="/home/$USER"
 TARGET_DIR="$USER_HOME/Documents/Server"
 
+cd ..
+mv VendingMachine "$TARGET_DIR"
 cd "$TARGET_DIR/WebControls" || exit 1
 python3 -m venv myenv
 source myenv/bin/activate
 pip install bottle
+pip install websockets pyserial
 deactivate
 
 
@@ -21,10 +23,10 @@ deactivate
 mkdir -p "$USER_HOME/.config/autostart"
 
 
-mv "$TARGET_DIR/machinekiosk.desktop" "$USER_HOME/.config/autostart/" || echo "Warning: machinekio>
+mv "$TARGET_DIR/machinekiosk.desktop" "$USER_HOME/.config/autostart/" 
 
-mv "$TARGET_DIR/serverbootup.service" /etc/systemd/system/ || echo "Warning: serverbootup.service >
-mv "$TARGET_DIR/websocket.service" /etc/systemd/system/ || echo "Warning: websocket.service not fo>
+mv "$TARGET_DIR/serverbootup.service" /etc/systemd/system/ 
+mv "$TARGET_DIR/websocket.service" /etc/systemd/system/ 
 
 
 systemctl daemon-reload
