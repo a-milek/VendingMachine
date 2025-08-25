ca48d2e24714d94f5ec9304fd2738f59b61d837f
Fearne <anna@milek.eu.org>
2025-08-25 09:29:30 +0100

    added missing files

--- Websocket/main.py
+++ Websocket/main.py
@@ -11,20 +11,23 @@ import time
 connected_clients = set()
 loop = asyncio.new_event_loop()
 asyncio.set_event_loop(loop)  # For access across threads
-
+screen_state_json = None
 
 class PrintLines(LineReader):
     def __init__(self):
         super().__init__()
+
         self.receiving = False
         self.received_lines = []
         self.lines_to_collect = 0
+
 
     def connection_made(self, transport):
         super(PrintLines, self).connection_made(transport)
         sys.stdout.write('Serial port opened\n')
 
     def handle_line(self, data):
+        global screen_state_json
         try:
             sys.stdout.write(f'Line received: {repr(data)}\n')
             line = data.strip()
@@ -42,18 +45,25 @@ class PrintLines(LineReader):
                 if self.lines_to_collect == 0:
                     combined_data = "\n".join(self.received_lines)
                     print("Sending to clients:", repr(combined_data))
-                    asyncio.run_coroutine_threadsafe(
-                        send_to_all_clients(json.dumps({
+
+                    screen_state_json=json.dumps({
                             "type": "serial",
                             "message": combined_data
-                        })),
-                        loop
-                    )
+                        })
+
+                    self._send_screen_state_to_clients()
                     self.received_lines = []
                     self.receiving = False
         except Exception:
             print("Exception in handle_line:")
             traceback.print_exc()
+
+    def _send_screen_state_to_clients(self):
+        asyncio.run_coroutine_threadsafe(
+            send_to_all_clients(screen_state_json),
+            loop
+        )
+
 
     def connection_lost(self, exc):
         print("Connection lost called")
@@ -66,6 +76,11 @@ class PrintLines(LineReader):
 async def websocket_handler(websocket):
     print("Client connected")
     connected_clients.add(websocket)
+    if screen_state_json:
+        await websocket.send(screen_state_json)
+
+
+
     try:
         async for message in websocket:
             print("Received from client:", message)
@@ -76,21 +91,20 @@ async def websocket_handler(websocket):
 
 
 async def start_websocket_server():
+    # test
     print("Starting WebSocket server on ws://localhost:8765/")
-    return await websockets.serve(websocket_handler, "localhost", 8765)
-
+    return await websockets.serve(websocket_handler,  "0.0.0.0", 8765)
 
 async def send_to_all_clients(message):
     if connected_clients:
         await asyncio.gather(*[client.send(message) for client in connected_clients])
-
 
 def main():
     ser = serial.Serial('/dev/serial/by-path/platform-xhci-hcd.0-usb-0:2:1.0-port0', baudrate=230440, timeout=1)
 
     while True:
         try:
-            with ReaderThread(ser, PrintLines) as protocol:
+            with ReaderThread(ser, PrintLines):
                 # Start the WebSocket server once per program run
                 ws_server = loop.run_until_complete(start_websocket_server())
                 print("WebSocket server running.")
