08aaa3dd6d7340ee9de663079437596c1635046a
a-milek <anna@milek.eu.org>
2026-04-14 13:39:42 +0200

    modified serial connection

--- Websocket/main.py
+++ Websocket/main.py
@@ -102,7 +102,8 @@ async def send_to_all_clients(message):
         await asyncio.gather(*[client.send(message) for client in connected_clients])
 
 def main():
-    ser = serial.Serial('/dev/serial/by-path/platform-xhci-hcd.0-usb-0:2:1.0-port0', baudrate=230440, timeout=1)
+    # ser = serial.Serial('rfc2217://rasp4-64-R.local:2006', baudrate=230440, timeout=1)
+    ser = serial.serial_for_url(os.getenv('VENDING_MACHINE_DEVICE_SERIAL_LCD'), baudrate=230440)
 
     while True:
         try:
