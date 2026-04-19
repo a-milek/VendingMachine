8fed0bc1305279d7c6f4530fdc3bc63880863d7d
a-milek <44343010+a-milek@users.noreply.github.com>
2026-04-19 21:16:49 +0200

    Update statisticsscript.sh

--- statisticsscript.sh
+++ statisticsscript.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 cd /home/amilek/Documents/Server/statisticsService
-. /home/amilek/Documents/Server/statisticsService/venv/bin/activate
-python3 -u /home/amilek/Documents/Server/statisticsService/main.py
+exec /home/amilek/Documents/Server/statisticsService/venv/bin/python3 -u main.py
 
